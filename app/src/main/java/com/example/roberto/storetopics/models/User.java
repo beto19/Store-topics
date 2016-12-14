cbd595db21c4fc50cbed6af2b4b98bc7998b1ac3
beto19 <beto.tellez19@gmail.com>
2016-12-14 13:04:40 -0600

    Modelos admin agregados

---
+++ app/src/main/java/com/example/roberto/storetopics/models/User.java
@@ -0,0 +1,36 @@
+package com.example.roberto.storetopics.models;
+
+/**
+ * Created by roberto on 14/12/16.
+ */
+
+public class User
+{
+    private String rol, nombre;
+    private  int id ;
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
